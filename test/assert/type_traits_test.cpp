a262152c0597651df876fc0e4415a3babdfc2002
Oleg Sych <oleg_sych@hotmail.com>
2015-07-26 08:58:54 -0700

    - Implement assert::is_abstract.

--- test/assert/type_traits_test.cpp
+++ test/assert/type_traits_test.cpp
@@ -12,7 +12,41 @@ namespace simply
             assert::implementation::fail,
             [&](const wstring& message) { output = message; }
         };
+
+        class abstract_type
+        {
+        public: 
+            virtual void method() = 0;
+        };
+
+        class concrete_type : public abstract_type 
+        {
+        public:
+            void method() override {}
+        };
+
     public:
+        #pragma region is_abstract<actual_t>()
+
+        TEST_METHOD(is_abstract_fails_when_type_is_concrete)
+        {
+            assert::is_abstract<concrete_type>();
+
+            Assert::AreNotEqual(wstring::npos, this->output.find(L"abstract"));
+            Assert::AreNotEqual(wstring::npos, this->output.find(L"concrete_type"));
+        }
+
+        TEST_METHOD(is_abstract_doesnt_fail_when_type_is_abstract)
+        {
+            assert::is_abstract<abstract_type>();
+
+            Assert::AreEqual<size_t>(0, this->output.length());
+        }
+
+        #pragma endregion
+
+        #pragma region is_same<expected_t, actual_t>()
+        
         TEST_METHOD(is_same_fails_when_types_are_different)
         {
             assert::is_same<int, double>();
@@ -28,5 +62,7 @@ namespace simply
             Assert::AreNotEqual(wstring::npos, this->output.find(L"volatile int"));
             Assert::AreNotEqual(wstring::npos, this->output.find(L"const int"));
         }
+
+        #pragma endregion
     };
 }
