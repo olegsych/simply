29100035b6b6f15454a4e8f5c6284abd4c9c5afe
Oleg Sych <oleg_sych@hotmail.com>
2015-07-28 14:14:07 -0700

    - Implement assert::is_equal<t> and assert::is_not_equal<t> functions.

--- src/simply/assert.h
+++ src/simply/assert.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <simply/assert/bool.h>
+#include <simply/assert/equality.h>
 #include <simply/assert/exception.h>
 #include <simply/assert/fail.h>
 #include <simply/assert/null.h>
