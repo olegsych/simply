9ae3c12de2d09045aebb9d2cfb67266140dfbe65
Oleg Sych <oleg_sych@hotmail.com>
2015-08-03 19:49:20 -0700

    - Change type_name and assert functions that use it to correctly report names of reference types.

--- src/simply/utility/type_name.h
+++ src/simply/utility/type_name.h
@@ -4,20 +4,31 @@
 
 namespace simply { namespace utility 
 {
-    template<typename t> const std::string type_name()
+    template<typename t> 
+    typename std::enable_if<!std::is_reference<t>::value, const std::string>::type
+    type_name()
     {
         std::string result { typeid(t).name() };
         
-        if (std::is_const<t>::value)
+        if (std::is_const<t>())
         {
             result.insert(0, "const ");
         }
 
-        if (std::is_volatile<t>::value)
+        if (std::is_volatile<t>())
         {
             result.insert(0, "volatile ");
         }
 
+        return result;
+    }
+
+    template<typename t> 
+    typename std::enable_if<std::is_reference<t>::value, const std::string>::type 
+    type_name()
+    {
+        std::string result = type_name<std::remove_reference<t>::type>();
+        result.append("&");
         return result;
     }
 }}
--- test/utility/type_name_test.cpp
+++ test/utility/type_name_test.cpp
@@ -8,10 +8,14 @@ namespace simply { namespace utility
     TEST_CLASS(type_name_test)
     {
     public:
-        TEST_METHOD(type_name_returns_const_string)
+        TEST_METHOD(type_name_for_non_reference_types_returns_const_string)
         {
-            Assert::IsTrue(typeid(string) == typeid(type_name<int>()));
-            Assert::IsTrue(is_const<decltype(type_name<int>())>::value);
+            Assert::IsTrue(is_same<const string, decltype(type_name<int>())>());
+        }
+
+        TEST_METHOD(type_name_for_reference_types_returns_const_string)
+        {
+            Assert::IsTrue(is_same<const string, decltype(type_name<int&>())>());
         }
 
         TEST_METHOD(type_name_returns_name_of_given_type)
@@ -27,6 +31,21 @@ namespace simply { namespace utility
         TEST_METHOD(type_name_return_name_of_given_volatile_type)
         {
             Assert::AreEqual(string { "volatile int" }, type_name<volatile int>());
+        }
+
+        TEST_METHOD(type_name_returns_name_of_given_reference_type)
+        {
+            Assert::AreEqual(string { "int&" }, type_name<int&>());
+        }
+
+        TEST_METHOD(type_name_returns_name_of_given_const_reference_type)
+        {
+            Assert::AreEqual(string { "const int&" }, type_name<const int&>());
+        }
+
+        TEST_METHOD(type_name_returns_name_of_given_volatile_reference_type)
+        {
+            Assert::AreEqual(string { "volatile int&" }, type_name<volatile int&>());
         }
     };
 }}
