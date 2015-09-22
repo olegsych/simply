9b215537450db66a8702ececc3b24ddd70de04ed
Oleg Sych <oleg_sych@hotmail.com>
2015-09-21 22:32:21 -0700

    - Move assert code to the new simply.assert repository.

--- src/assert/simply/assert.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <simply/assert/bool.h>
-#include <simply/assert/equality.h>
-#include <simply/assert/exception.h>
-#include <simply/assert/fail.h>
-#include <simply/assert/null.h>
-#include <simply/assert/string.h>
-#include <simply/assert/type_traits.h>
